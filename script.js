9921a5ba267da145a9875d3cdea8652180767e1b
Roland Levy <contact@rolandlevy.co.uk>
2024-11-08 14:26:05 +0000

    added movement with keypress

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   const $ = (elem) => document.querySelector(elem);
 
   const createElem = (tagName, props = {}) => {
@@ -6,15 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
     return Object.assign(el, props);
   };
 
-  const DEFAULT_SIZE = 5;
-
-  const getSize = () => {
+  const getSearchParamValue = (key) => {
     const url = new URL(window.location.href);
     const params = new URLSearchParams(url.search);
-    return (sizeParam = params.get('size'));
+    return Number(params.get(key));
   };
 
-  const size = getSize() || DEFAULT_SIZE;
+  const DEFAULT_SIZE = 10;
+  const size = getSearchParamValue('size') || DEFAULT_SIZE;
   const maxSize = size * size;
 
   $('.grid-container').style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -22,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const matrix = Array(maxSize).fill({});
 
-  matrix.forEach((item, index) => {
+  matrix.forEach((_, index) => {
     const gridItemProps = {
       className: 'grid-item',
       textContent: index + 1,
@@ -34,6 +33,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const getRandomNum = (max) => Math.floor(Math.random() * max) + 1;
 
+  const getNextPos = (direction, i) => {
+    const sizePlus = i + size;
+    const sizeMinus = i - size;
+    const nextPos = {
+      ArrowUp: sizeMinus < 0 ? null : sizeMinus,
+      ArrowDown: sizePlus >= maxSize ? null : sizePlus,
+      ArrowLeft: i % size === 0 ? null : i - 1,
+      ArrowRight: (i + 1) % (size === 0) ? null : i + 1,
+    };
+    return nextPos[direction];
+  };
+
+  const moveBlock = (direction) => {
+    const activeItem = $('.active');
+    const [indexString] = activeItem.id.match(/\d+/);
+    const index = Number(indexString) - 1;
+    const nextItemIndex = getNextPos(direction, index);
+    if (nextItemIndex !== null) {
+      activeItem.classList.remove('active');
+      $(`#item_${nextItemIndex + 1}`).classList.add('active');
+    }
+  };
+
+  document.addEventListener('keydown', (e) => {
+    moveBlock(e.key);
+  });
+
   const highlightRandomItem = () => {
     const randItemNum = String(getRandomNum(maxSize));
     $('.active')?.classList.remove('active');
@@ -41,6 +67,4 @@ document.addEventListener('DOMContentLoaded', function () {
   };
 
   highlightRandomItem();
-
-  setInterval(highlightRandomItem, 200);
 });
